7be36a0483f691764e80c1869e909bfe42e8e0f4
Mohosin999 <programmermohosin@gmail.com>
2023-05-29 10:54:15 +0600

    test code successfully running

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const add = () => {};
+const add = (a, b) => {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Invalid Argument Type");
+  }
+  return a + b;
+};
 
 module.exports = {
   add,
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,1 +1,11 @@
 const { add } = require("../app");
+
+test("should return correct addition value", () => {
+  expect(add(2, 3)).toEqual(5);
+  expect(add(8, 2)).toEqual(10);
+  expect(add(80, 20)).toEqual(100);
+});
+
+test("should throw an error if arguments are not number", () => {
+  expect(() => add("A", 5)).toThrow();
+});
