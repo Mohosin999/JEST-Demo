fde6a6109e1c04a31185527670a479e96ea02c1f
Mohosin999 <programmermohosin@gmail.com>
2023-05-30 08:13:23 +0600

    JEST Testing

--- src/getWords.js
+++ src/getWords.js
@@ -1,4 +1,11 @@
-const getWords = (sentence) => {};
+const getWords = (sentence) => {
+  if (typeof sentence !== "string") {
+    throw new Error("Invalid Argument Type");
+  }
+
+  const words = sentence.split(" ");
+  if (words.length === 1) return sentence;
+};
 
 module.exports = {
   getWords,
--- tests/getWords.test.js
+++ tests/getWords.test.js
@@ -1,1 +1,28 @@
 const { getWords } = require("../src/getWords");
+
+test("should throw an error if it is not string", () => {
+  expect(() => getWords(100)).toThrow();
+  expect(() => getWords(true)).toThrow();
+  expect(() => getWords(false)).toThrow();
+  expect(() => getWords(null)).toThrow();
+  expect(() => getWords(undefined)).toThrow();
+  expect(() => getWords(["words"])).toThrow();
+  expect(() => getWords({ name: "words" })).toThrow();
+  expect(() => getWords()).toThrow();
+});
+
+test("should return the same string if the argument is a word", () => {
+  expect(getWords("Mohosin")).toBe("Mohosin");
+  expect(getWords("TypeScript")).toBe("TypeScript");
+  expect(getWords("Jest")).toBe("Jest");
+});
+
+test("should return the array of string", () => {
+  const words = getWords("Mohosin Hasan");
+  expect(words).toHaveLength(2);
+  expect(words).toContain("Mohosin");
+
+  const words = getWords("Test Driven Development");
+  expect(words).toHaveLength(3);
+  expect(words).toContain("Test");
+});
